4a75afc9c27d771f37ff1ec2cd32643f926728ad
Grimcat07 <grimcat07@gmail.com>
2025-01-31 19:24:12 +0530

    added button function to change grid boxes

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-let box=16;
 let divs=[];
+let num=0;
 const container=document.querySelector(".container");
-function createBox(box){
-    boxsquare=box*box;
-    squarebox=600/box;
+const boxes=document.querySelector(".boxes");
+boxes.addEventListener("click",()=>{num=Number(prompt('Enter the number of grids less than 100'));
+    if(num<100)
+    {
+        container.innerHTML="";
+        divs=[];
+        createBox(num);
+    }
+    else
+    {
+        container.innerHTML="";
+        divs=[];
+        createBox(16);
+        alert('enter the correct number');
+    }
+});
+
+function createBox(num){
+    boxsquare=num*num;
+    let div;
+    squarebox=600/num;
     while(boxsquare>0)
     {
-        let div=document.createElement("div");
+        div=document.createElement("div");
         div.classList.add('grid-box');
         div.style.width=`${squarebox}px`;
         div.style.height=`${squarebox}px`;
         divs.push(div);
         container.appendChild(div);
         boxsquare--;
-    }    
+    }
 }
-const boxes=document.querySelector(".boxes");
-boxes.addEventListener("click",()=>{prompt('Enter the number of grids')});
-createBox(box);
+createBox(16);
+console.log(divs);
+divs.forEach((div)=>{
+    div.addEventListener("mouseover",()=>
+        {div.style.backgroundColor="white"});
+    div.addEventListener("mouseout",()=>
+    {
+        div.style.backgroundColor="black";
+    });
+    
+});
